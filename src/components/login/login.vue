09694543af7c9c763bc585004587a6a246972f28
胡浩 <qwhuhaopp520@163.com>
2019-10-22 15:07:11 +0800

    新建登录分支以及组件

---
+++ src/components/login/login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="loginC">
+    <p>测试登录</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+const Login = () => import('@/components/login/login')
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/'
+      name: 'login',
+      path: '/login',
+      component: Login
     }
   ]
 })
